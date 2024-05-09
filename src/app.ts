63aa579d9e472be75ec62a5ef21b6b4b7f7304dc
abijith <abijithsurendran005@gmail.com>
2024-05-09 23:54:35 +0530

    register user completed

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express"
 import helmet from "helmet"
 import morgan from "morgan"
 import "dotenv/config"
-import dbConnect  from "./config/dbConfig.js"
-import userRouter from "./routers/authRoutes.js"
+import dbConnect  from "./config/dbConfig"
+import userAuthRouter from "./routers/authRoutes"
 
 const port=process.env.PORT ||4000
 const app=express()
@@ -12,9 +12,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(helmet())
+ 
+app.use('/api/v1/auth',userAuthRouter) 
 
-app.use('/api/v1/auth',userRouter)
-
-app.listen(port,()=>{
+app.listen(port,()=>{ 
     console.log(`server runninon port ${port}`);
-})
+})  
